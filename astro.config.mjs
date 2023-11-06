b2b59dc946850fc52eb25f8ee4bffc3276ddf0e0
Cristian Bilu <cristian@eptic.ro>
2023-11-06 15:41:26 +0200

    Remove unwanted deps and add deno as a deployment adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: deno(),
 });
